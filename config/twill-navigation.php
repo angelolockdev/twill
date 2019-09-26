013ec1851b232366562d2990cae15762c40cbb65
Angelo <a.locktohang@fulldigits.com>
2019-09-26 11:42:00 +0300

    after publishing vendor we have got an error to display the styles

--- config/twill-navigation.php
+++ config/twill-navigation.php
@@ -4,5 +4,24 @@ return [
     'products' => [
         'title' => 'Products',
         'module' => true
+    ],
+    'work' => [
+        'title' => 'Work', 
+        'primary_navigation' => [
+            'title' => 'Projects',
+            'module' => false,
+        ],
+        'clients' => [
+            'title' => 'Clients',
+            'module' => false,
+        ],
+        'industries' => [
+            'title' => 'Industries',
+            'module' => false,
+        ],
+        'studios' => [
+            'title' => 'Studios',
+            'module' => false,
+        ]
     ]
 ];
